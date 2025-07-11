2f6d8fab1cc96d25342263779a42e984d0321560
PatricioQuiroga23 <paqt1988@gmail.com>
2025-07-10 20:04:59 -0400

    Initial commit

---
+++ funcionar git en r.R
@@ -0,0 +1,17 @@
+y
+
+usethis::use_git()
+
+install.packages("renv")
+
+##use_git_config(user.name = "PatricioQuiroga23", user.email = "paqt1988@gmail.com")
+
+usethis::use_github()
+
+usethis::create_github_token()
+
+usethis::edit_r_environ()
+
+usethis::git_sitrep()
+
+
