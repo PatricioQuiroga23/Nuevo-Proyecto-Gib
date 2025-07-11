ff127c100e5548812e505be9082f4b3dabd9b36f
PatricioQuiroga23 <paqt1988@gmail.com>
2025-07-10 22:30:11 -0400

    Initial commit

--- funcionar git en r.R
+++ funcionar git en r.R
@@ -7,6 +7,8 @@ usethis::use_git()
 
 install.packages("renv")
 
+install.packages(c("usethis", "renv", "gh"))
+
 use_git_config(user.name = "PatricioQuiroga23", user.email = "paqt1988@gmail.com")
 
 usethis::use_github()
@@ -16,5 +18,13 @@ usethis::create_github_token()
 usethis::edit_r_environ()
 
 usethis::git_sitrep()
+
+library(tesseract)
+
+eng <- tesseract("eng")
+
+text <- tesseract::ocr("http://jeroen.github.io/images/testocr.png", engine = eng)
+
+cat(text)
 
 
