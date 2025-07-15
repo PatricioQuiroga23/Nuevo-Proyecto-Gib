e8e3b89295b31e1866411038277a5e4e92e27723
PatricioQuiroga23 <paqt1988@gmail.com>
2025-07-14 20:50:47 -0400

    Initial commit

--- ejemplo ocr.R
+++ ejemplo ocr.R
@@ -59,4 +59,20 @@ names(sector_profile) <- c("sector",
                            "deaths")
 
 
+##
+
+library(tesseract)
+
+eng <- tesseract("eng")
+
+text <- tesseract::ocr("http://jeroen.github.io/images/testocr.png", engine = eng)
+
+cat(text)
+
+pngfile <- pdftools::pdf_convert('https://jeroen.github.io/images/ocrscan.pdf', dpi = 600)
+
+text <- tesseract::ocr(pngfile)
+
+cat(text)
+
 
